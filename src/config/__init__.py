0e99244d7969903b4df5b9d054da8d5fd866bae7
ilia_volodin <vollodin61@gmail.com>
2025-04-12 20:59:03 +0300

    Base config

---
+++ src/config/__init__.py
@@ -0,0 +1,11 @@
+from .conf import (
+                   Emo,
+                   BaseConfig,
+                   RedisConfig,
+                   env,
+                   info_logger,
+                   error_logger,
+                   warning_logger,
+                   exception_logger,
+                   debug_logger
+                   )
