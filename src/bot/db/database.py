802acc772c2aa19a7d41f0892f13db9f00a1436b
ilia_volodin <vollodin61@gmail.com>
2025-04-15 13:12:11 +0300

    Хендлер для файла с базовой валидацией, отображением пользователю первых 20 записей, кастомизацией ошибки, сохранением в базу.

--- src/bot/db/database.py
+++ src/bot/db/database.py
@@ -1,5 +1,6 @@
 import asyncio  # noqa
 
+import pandas as pd
 from sqlalchemy import insert
 
 from src.bot.db.db_config import DBConfig
@@ -32,7 +33,8 @@ async def add_line_to_data_from_tables(model_schema: DataFromTablesSchema):
     """
     Продвинутый способ. Уже возвращаем id, если понадобится дальнейшее взаимодействие.
     Но работает только с конкретной моделью. Достаточно для маленьких проектов.
-    Не буду делать избыточный унифицированный сервис через зависимости, репозитории. KISS
+    Не буду делать избыточный унифицированный сервис с зависимостями, абстрактными классами,
+    собственным контекстным менеджером, который прокидывает и управляет сессией. KISS
     :param model_schema:
     :return: id добавленной записи
     """
@@ -46,7 +48,55 @@ async def add_line_to_data_from_tables(model_schema: DataFromTablesSchema):
             info_logger(f'В базу добавлена запись {model_schema}')
             return res.scalar_one_or_none()
         except Exception as e:
+            await session.rollback()
             error_logger(f'Не получилось добавить запись в таблицу: {model_schema}\n'
                          f'Ошибка {repr(e)}')
+
+
+async def save_data_from_tables_to_db(file_path, chunk_size: int = 100):
+    """
+    Сохраняем данные из таблицы в базу, будем пачками запихивать
+
+    :param file_path: путь к файлу, который прислал пользователь и мы его уже проверили на валидность и сохранили
+    :param chunk_size: размер пакета для вставки в бд
+    :return:
+    """
+    df = pd.read_excel(file_path)
+
+    #  Вначале преобразуем
+    try:
+        records = [
+            DataFromTablesSchema(
+                title=row['title'],
+                url=row['url'],
+                xpath=row['xpath']
+            )
+            for _, row in df.iterrows()
+        ]
+    except Exception as e:
+        error_logger(f'Ошибка преобразования данных: {repr(e)}')
+
+    success = 0
+    errors = 0
+
+    async with DBConfig.async_session_factory() as session:
+        for i in range(0, len(records), chunk_size):
+            chunk = records[i:i + chunk_size]
+
+            try:
+                chunk_dicts = [item.model_dump() for item in chunk]
+
+                await session.execute(
+                    insert(DataFromTablesModel), chunk_dicts
+                )
+                await session.commit()
+                success += len(chunk)
+            except Exception as e:
+                await session.rollback()
+                errors += len(chunk)
+                error_logger(f"Ошибка при вставке данных в базу: {repr(e)}")
+                continue
+    info_logger(f'\nУспешно добавлено: {success}\n'
+                f'Ошибок при добавлении: {errors}')
 
 # asyncio.run(create_tables())
--- src/bot/handlers/default.py
+++ src/bot/handlers/default.py
@@ -1,10 +1,52 @@
-from aiogram import Router
-from aiogram.filters import CommandStart
+import os
+from datetime import datetime
+
+import pandas as pd
+from aiogram import Router, Bot
+from aiogram.filters import CommandStart, StateFilter
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
+
+from src.bot.config.bot_config import MyStates
+from src.bot.db.database import save_data_from_tables_to_db
+from src.bot.utils.keyboards import add_file_keyboard_button
+from src.bot.utils.utils import get_file_data_for_answer, validate_file_structure
+from src.config import error_logger
 
 def_router = Router()
 
 
 @def_router.message(CommandStart())
-async def cmd_start_handler(msg: Message):
-    await msg.answer("Hello, my friend! 🤗\n\nНажми на кнопку, получишь результат! 😉")
+async def cmd_start_handler(msg: Message, state: FSMContext):
+    await msg.answer("Hello, my friend! 🤗\n\nНажми на кнопку, получишь результат! 😉",
+                     reply_markup=await add_file_keyboard_button())
+    await state.set_state(MyStates.wait_file_for_parse)
+
+
+@def_router.message(StateFilter(MyStates.wait_file_for_parse))
+async def wait_file_for_parse_handler(msg: Message, state: FSMContext, bot: Bot):
+    if not msg.document.file_name.endswith(('.xlsx', '.xls')) or not msg.document:
+        await msg.answer("Это не то, что я ожидаю)) Пришли файл Excel-формата")
+        return
+
+    dt_now = datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_path = f'temp/{msg.chat.id}_{dt_now}_{msg.document.file_name}'
+    os.makedirs("temp", exist_ok=True)
+
+    file_info = await bot.get_file(msg.document.file_id)
+    await bot.download_file(file_info.file_path, file_path)
+
+    try:
+        await validate_file_structure(file_path)
+    except Exception as e:
+        await msg.answer(f"Файл не является корректным Excel-файлом. Ошибка {str(e)}")
+        error_logger(f"Файл не является корректным Excel-файлом. Ошибка {repr(e)}")
+        os.remove(file_path)
+        return
+
+    full_file_path = os.path.abspath(file_path)
+    file_data_for_answer = await get_file_data_for_answer(full_file_path)
+    await msg.answer('Файл получен и проверен успешно. Вот его содержание:')
+    await msg.answer(file_data_for_answer)
+    await state.clear()
+    await save_data_from_tables_to_db(full_file_path)
